fcc68adc384f45bdabccc20baffe564e7c52f7c1
Rafał Rudko <rafal.rudko@rb.com>
2023-01-27 23:00:13 +0100

    Project init with expressjs

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express()
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('check')
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
